fe2e174a9cd1522701adf2ef089e3c58d0756ac5
programindfr <mathias.rivet73@orange.fr>
2023-11-03 18:14:15 +0100

    add stamina v1

--- src/Actors/Player.cs
+++ src/Actors/Player.cs
@@ -1,36 +1,82 @@
 using Godot;
+using System;
 
 public partial class Player : CharacterBody2D
 {
-	// Signals
-	// Start using stamina
-	[Signal]
-	public delegate void StartStaminaEventHandler();
-	// Stop using stamina
-	[Signal]
-	public delegate void StopStaminaEventHandler();
-	
 	// Variables publiques
 	// Vitesse du joueur
 	[Export]
 	public int Speed { get; set; } = 400;
 	// Valeur du bonnus d'endurance
 	[Export]
-	public float StaminaSpeedMultiplierValue { get; set; } = 1.05F;
+	public double StaminaSpeedMultiplierValue { get; set; } = 1.15D;
+	// Valeur du temps d'endurance
+	[Export]
+	public double StaminaTimeValue { get; set; } = 3.0D;
+	
 	
 	// Variables internes
 	// Bonnus d'endurance
-	private float _staminaSpeedMultiplier;
+	private double _staminaSpeedMultiplier = 1;
 	
-	public override void _Ready()
+	
+	// Fonctions internes
+	private bool _start_stamina_consumption()
 	{
+		var timer = GetNode<Timer>("StaminaConsumptionTimer");
+		if (timer.Paused)
+		{
+			timer.Paused = false;
+			_stop_stamina_cooldown();
+			_stop_stamina_regeneration();
+			return true;
+		}
+		return false;
 	}
 	
+	private void _stop_stamina_consumption()
+	{
+		GetNode<Timer>("StaminaConsumptionTimer").Paused = true;
+		_start_stamina_cooldown();
+	}
+	
+	private void _start_stamina_cooldown()
+	{
+		GetNode<Timer>("StaminaCooldownTimer").Start();
+	}
+	
+	private void _stop_stamina_cooldown()
+	{
+		GetNode<Timer>("StaminaCooldownTimer").Stop();
+	}
+	
+	private void _start_stamina_regeneration(double timeLeft)
+	{
+		GetNode<Timer>("StaminaRegenerationTimer").Start(timeLeft);
+	}
+	
+	private void _stop_stamina_regeneration()
+	{
+		GetNode<Timer>("StaminaRegenerationTimer").Stop();
+	}
+	
+	
+	// _Ready function
+	public override void _Ready()
+	{
+		var timer = GetNode<Timer>("StaminaConsumptionTimer");
+		timer.Paused = true;
+		timer.Start();
+	}
+	
+	
+	// _Process function
 	public override void _Process(double delta)
 	{
 		// Vecteur déplassement du joueur
 		Vector2 velocity = Vector2.Zero;
 
+		// Controles déplacement du joueur
 		if (Input.IsActionPressed("player_right"))
 		{
 			velocity.X += 1;
@@ -53,20 +99,24 @@ public partial class Player : CharacterBody2D
 		
 		if (Input.IsActionPressed("player_sprint"))
 		{
-			EmitSignal(SignalName.StartStamina);
-			_staminaSpeedMultiplier = StaminaSpeedMultiplierValue;
+			if (_start_stamina_consumption())
+			{
+				_staminaSpeedMultiplier = StaminaSpeedMultiplierValue;
+			}
 		}
-		else if (_staminaSpeedMultiplier != 1)
+		// _staminaSpeedMultiplier != 1
+		else if (Math.Abs(_staminaSpeedMultiplier - 1) > 0.001D)
 		{
-			EmitSignal(SignalName.StopStamina);
 			_staminaSpeedMultiplier = 1;
+			_stop_stamina_consumption();
 		}
 
+		// Gestion de l'annimation du joueur
 		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 
 		if (velocity.Length() > 0)
 		{
-			velocity = velocity.Normalized() * Speed * _staminaSpeedMultiplier;
+			velocity = velocity.Normalized() * Speed * (float)_staminaSpeedMultiplier;
 			
 			if (velocity.X < 0)
 			{
@@ -94,10 +144,42 @@ public partial class Player : CharacterBody2D
 		}
 		
 		MoveAndCollide(velocity * (float)delta);
+		
+		// Gestion de l'endurance
+		var regenerationTimer = GetNode<Timer>("StaminaRegenerationTimer");
+		var consumptionTimer = GetNode<Timer>("StaminaConsumptionTimer");
+		if (!regenerationTimer.IsStopped())
+		{
+			consumptionTimer.Start(StaminaTimeValue - regenerationTimer.TimeLeft);
+		}
+		
+		var bar = GetNode<ProgressBar>("ProgressBar");
+		bar.Value = consumptionTimer.TimeLeft;
+		
+		//GD.Print(GetNode<Timer>("StaminaConsumptionTimer").TimeLeft);
 	}
 	
+	
+	// Signal function's
 	private void _on_stamina_consumption_timer_timeout()
 	{
 		_staminaSpeedMultiplier = 1;
+		_start_stamina_cooldown();
+	}
+	
+	private void _on_stamina_cooldown_timer_timeout()
+	{
+		var timer = GetNode<Timer>("StaminaConsumptionTimer");
+		timer.Paused = true;
+		if (Math.Abs(timer.TimeLeft) < 0.001D)
+		{
+			timer.Start(0.001D);
+			_start_stamina_regeneration(StaminaTimeValue);
+		}
+		else
+		{
+			timer.Start(timer.TimeLeft);
+			_start_stamina_regeneration(StaminaTimeValue - timer.TimeLeft);
+		}
 	}
 }
